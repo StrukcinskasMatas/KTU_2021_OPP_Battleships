9fea2b90e3dcfff9d3ffe911792daad0541498c6
Mecholaz <strukcinskas.matas@gmail.com>
2021-10-03 19:05:56 +0300

    Changes

--- Server/Server/Game.cs
+++ Server/Server/Game.cs
@@ -23,8 +23,62 @@ namespace Server
             SendGlobalMessage("(cls) Game is starting!");
             SetupShips();
 
-            // TODO: next things
-            Console.WriteLine("This is it, nothing here?!");
+            int activePlayerID = 0;
+            int winnerID = grid.GetWinnerID();
+            while (winnerID == -1)
+            {
+                // Define players
+                Player activePlayer = players[activePlayerID];
+                Player waitingPlayer = players[1 - activePlayerID];
+
+                // Inform waiting player
+                waitingPlayer.SendMessage("Waiting for other player's turn...");
+
+                // Get command from active player
+                activePlayer.SendMessage("(cls)");
+                activePlayer.SendMessage(this.grid.PrintGrid(activePlayerID));
+                activePlayer.SendMessage("(action needed) Attack enemy's territory: (example input: \"1 2\")");
+
+                while (true)
+                {
+                    List<int> coords = ConvertResponseToCoordsList(activePlayer.ReceiveMessage());
+
+                    // TODO: validate input based on the grid (if not out of bounds, if the sub-grid belongs to the player)
+                    if (coords.Count == 2)
+                    {
+                        Console.WriteLine("Player " + activePlayerID.ToString() + " attacked: " + coords[0].ToString() + ", " + coords[1].ToString());
+                        Cell cell = grid.GetCell(coords[0], coords[1]);
+                        if (cell.GetOwnerID() == activePlayerID)
+                        {
+                            activePlayer.SendMessage("(action needed) You cannot attack your own territory.");
+                            continue;
+                        }
+
+                        cell.AddHit();
+                        activePlayer.SendMessage("(cls)");
+                        activePlayer.SendMessage(this.grid.PrintGrid(activePlayerID));
+                    }
+                    else
+                    {
+                        activePlayer.SendMessage("(action needed) Invalid input.");
+                        continue;
+                    }
+
+                    // Get out of the while loop after successful attack (replace with a bool?)
+                    break;
+                }
+
+                // Invert the active player's ID
+                activePlayerID = Math.Abs(activePlayerID - 1);
+
+                // Check game state
+                winnerID = grid.GetWinnerID();
+            }
+
+            players[winnerID].SendMessage("You've won the game!");
+            players[1 - winnerID].SendMessage("You've lost the game!");
+
+            Console.WriteLine("Game ended!");
         }
 
         private void SetupShips()
@@ -41,18 +95,19 @@ namespace Server
         private void PlaceShips(int playerID)
         {
             Player player = players[playerID];
-            player.SendMessage(this.grid.PrintGrid());
+            player.SendMessage("(cls)");
+            player.SendMessage(this.grid.PrintGrid(playerID));
 
             for (int i = 0; i < 3; i++)
             {
                 player.SendMessage("(action needed) Place your 1x1 ship: (example input: \"1 2\")");
-                string response = player.ReceiveMessage();
+                List<int> coords = ConvertResponseToCoordsList(player.ReceiveMessage());
 
-                // TODO: validate input based on the grid (if not out of bounds, if the sub-grid belongs to the player)
-                var coords = response.Split(" ").Where(x => int.TryParse(x, out _)).Select(int.Parse).Select(x => x - 1).ToList();
+                // TODO: validate input if not out of bounds
+                // TODO: validate if ship already placed
                 if (coords.Count == 2)
                 {
-                    Console.WriteLine("Should place 1x1 ship at: " + coords[0].ToString() + " and " + coords[1].ToString());
+                    Console.WriteLine("Player " + playerID.ToString() + " placed 1x1 ship at: " + coords[0].ToString() + ", " + coords[1].ToString());
                     Cell cell = grid.GetCell(coords[0], coords[1]);
                     if (cell.GetOwnerID() != playerID)
                     {
@@ -63,12 +118,13 @@ namespace Server
 
                     cell.SetValue('1');
                     player.SendMessage("(cls)");
-                    player.SendMessage(this.grid.PrintGrid());
+                    player.SendMessage(this.grid.PrintGrid(playerID));
                 }
                 else
                 {
                     player.SendMessage("Invalid input.");
                     i--;
+                    continue;
                 }
             }
         }
@@ -79,6 +135,16 @@ namespace Server
             {
                 player.SendMessage(message);
             }
+        }
+
+        private List<int> ConvertResponseToCoordsList(string response)
+        {
+            return response
+                .Split(" ")
+                .Where(x => int.TryParse(x, out _))
+                .Select(int.Parse)
+                .Select(x => x - 1)
+                .ToList();
         }
     }
 }
--- Server/Server/GameGrid.cs
+++ Server/Server/GameGrid.cs
@@ -30,28 +30,75 @@ namespace Server
             return grid[x, y];
         }
 
-        public string PrintGrid()
+        public string PrintGrid(int playerID)
         {
-            string gridVisuals = "  ";
-            for (int i = 0; i < size; i++)
-            {
-                gridVisuals += (i + 1).ToString();
-            }
-            gridVisuals += "\n\n";
+            string gridVisuals = "";
 
             for (int i = 0; i < size; i++)
             {
-                gridVisuals += (i + 1).ToString() + " ";
-
                 for (int j = 0; j < size; j++)
                 {
-                    gridVisuals += grid[i, j].GetValue().ToString();
+                    if (playerID == grid[i, j].GetOwnerID())
+                    {
+                        gridVisuals += grid[i, j].GetValue().ToString();
+                    }
+                    else
+                    {
+                        gridVisuals += grid[i, j].GetHits() == 0 ? 'o' : 'x';
+                    }
                 }
 
                 gridVisuals += "\n";
             }
 
             return gridVisuals;
+        }
+
+
+
+        // returns -1 if there's no winner
+        // otherwise, returns the winners ID
+        public int GetWinnerID()
+        {
+            // Run through player 0 cells
+            int aliveShips = 0;
+            for (int i = 0; i < size / 2; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Cell cell = grid[i, j];
+                    if (cell.GetHits() == 0 && cell.GetValue() != '0')
+                    {
+                        aliveShips++;
+                    }
+                }
+            }
+            if (aliveShips == 0)
+            {
+                // player 1 won, since player 0 got no ships alive
+                return 1;
+            }
+
+            aliveShips = 0;
+            // Run through player 1 cells
+            for (int i = size / 2; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Cell cell = grid[i, j];
+                    if (cell.GetHits() == 0 && cell.GetValue() != '0')
+                    {
+                        aliveShips++;
+                    }
+                }
+            }
+            if (aliveShips == 0)
+            {
+                // player 0 won, since player 1 got no ships alive
+                return 0;
+            }
+
+            return -1;
         }
     }
 
